47c923c5b73168ab727ba2bed5511e3c1487ff74
ashish <aks3ashish@gmail.com>
2022-09-27 21:33:18 +0600

    cors add

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const postRoute = require("./routes/posts");
 const categoryRoute = require("./routes/categories");
 const multer = require("multer");
 const path = require("path");
+const cors = require('cors')
 
 dotenv.config();
 app.use(express.json());
@@ -22,6 +23,8 @@ mongoose
   })
   .then(console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
+  
+  app.use(cors())
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
